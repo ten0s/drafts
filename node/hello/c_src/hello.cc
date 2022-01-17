13020ba592d143c50ea1835614841f84ffbb7cf8
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-01-17 17:34:10 +0300

    Implement Sum function

--- node/hello/c_src/hello.cc
+++ node/hello/c_src/hello.cc
@@ -13,9 +13,40 @@ void Hello(const FunctionCallbackInfo<Value>& args) {
     retVal.Set(checkedStr);
 }
 
+void Sum(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    double sum = 0.0;
+    for (int i = 0; i< args.Length(); ++i) {
+        if (args[i]->IsNumber()) {
+            sum += args[i].As<Number>()->Value();
+        } else {
+            Local<String> argType = args[i]->TypeOf(isolate);
+
+            const unsigned typeBufSz = 256;
+            char typeBuf[typeBufSz];
+            argType->WriteUtf8(isolate, typeBuf, typeBufSz);
+
+            const unsigned msgBufSz = 1024;
+            char msgBuf[msgBufSz];
+            snprintf(msgBuf, msgBufSz, "Expected %s, but saw %s", "number", typeBuf);
+
+            Local<String> msg = String::NewFromUtf8(
+                isolate, msgBuf, NewStringType::kNormal).ToLocalChecked();
+            isolate->ThrowException(Exception::TypeError(msg));
+            return;
+        }
+    }
+
+    Local<Number> res = Number::New(isolate, sum);
+    ReturnValue<Value> retVal = args.GetReturnValue();
+    retVal.Set(res);
+}
+
 // Add addons must export an initialization function
 void Initialize(Local<Object> exports) {
     NODE_SET_METHOD(exports, "hello", Hello);
+    NODE_SET_METHOD(exports, "sum", Sum);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize) // no semi-colon here!
--- node/hello/src/main.js
+++ node/hello/src/main.js
@@ -1,2 +1,14 @@
 const addon = require('../build/Release/hello.node');
+
 console.log(addon.hello());
+
+console.log('0 = ', addon.sum());
+console.log('1 = ', addon.sum(1));
+console.log('1 + 2 = ', addon.sum(1, 2));
+console.log('1 + 2 + 3 = ', addon.sum(1, 2, 3));
+try {
+    console.log('1 + "2" + 3 = ');
+    addon.sum(1, '2', 3);
+} catch (e) {
+    console.error(e);
+}
