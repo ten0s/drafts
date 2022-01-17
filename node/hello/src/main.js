fb39cf245c9c937728dc5734d1e1ab4fde498399
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-01-17 18:50:44 +0300

    Refine

--- node/hello/src/main.js
+++ node/hello/src/main.js
@@ -1,14 +1,19 @@
-const addon = require('../build/Release/hello.node');
+const addon = require('../build/Release/hello.node')
 
-console.log(addon.hello());
+console.log(addon.hello())
 
-console.log('0 = ', addon.sum());
-console.log('1 = ', addon.sum(1));
-console.log('1 + 2 = ', addon.sum(1, 2));
-console.log('1 + 2 + 3 = ', addon.sum(1, 2, 3));
+console.log('0 = ', addon.sum())
+console.log('1 = ', addon.sum(1))
+console.log('1 + 2 = ', addon.sum(1, 2))
+console.log('1 + 2 + 3 = ', addon.sum(1, 2, 3))
 try {
-    console.log('1 + "2" + 3 = ');
-    addon.sum(1, '2', 3);
+    console.log('1 + "2" + 3 = ')
+    addon.sum(1, '2', 3)
 } catch (e) {
-    console.error(e);
+    if (e.message !== 'Expected number, but saw string') {
+        throw e
+    }
+    console.error(e.message)
 }
+
+console.log('OK')
