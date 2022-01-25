c2f7aeeca8faa599e1e0a2a15b14b25b79c930fa
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-01-25 22:30:33 +0300

    Refactor

--- node/4-rainfall/c_src/rainfall_node.cc
+++ node/4-rainfall/c_src/rainfall_node.cc
@@ -26,39 +26,55 @@ using v8::Value;
 
 #define NEW_STR(str) String::NewFromUtf8(isolate, str, NewStringType::kNormal).ToLocalChecked()
 
-Sample unpack_sample(Isolate* isolate, const Local<Context>& context, Local<Object>& Obj) {
+Sample unpack_sample(Isolate* isolate,
+                     const Local<Context>& context,
+                     const Local<Object>& obj) {
     Sample s;
 
-    Local<Value> Date = Obj->Get(context, NEW_STR("date")).ToLocalChecked();
-    Local<Value> Rainfall = Obj->Get(context, NEW_STR("rainfall")).ToLocalChecked();
+    Local<Value> date = obj->Get(context, NEW_STR("date")).ToLocalChecked();
+    Local<Value> rainfall = obj->Get(context, NEW_STR("rainfall")).ToLocalChecked();
 
-    String::Utf8Value str(isolate, Date);
+    String::Utf8Value str(isolate, date);
     s.date = std::string(*str);
-    s.rainfall = Rainfall.As<Number>()->Value();
+    s.rainfall = rainfall.As<Number>()->Value();
 
     return s;
 }
 
-Location unpack_location(Isolate* isolate, const Local<Context>& context, const Local<Object>& Obj) {
+Location unpack_location(Isolate* isolate,
+                         const Local<Context>& context,
+                         const Local<Object>& obj) {
     Location loc;
 
-    Local<Value> Lat = Obj->Get(context, NEW_STR("latitude")).ToLocalChecked();
-    Local<Value> Lon = Obj->Get(context, NEW_STR("longitude")).ToLocalChecked();
-    loc.latitude = Lat.As<Number>()->Value();
-    loc.longitude = Lon.As<Number>()->Value();
+    Local<Value> lat = obj->Get(context, NEW_STR("latitude")).ToLocalChecked();
+    Local<Value> lon = obj->Get(context, NEW_STR("longitude")).ToLocalChecked();
+    loc.latitude = lat.As<Number>()->Value();
+    loc.longitude = lon.As<Number>()->Value();
 
-    Local<Array> Arr = Local<Array>::Cast(Obj->Get(context, NEW_STR("samples")).ToLocalChecked());
-    size_t len = Arr->Length();
+    Local<Array> arr = Local<Array>::Cast(obj->Get(context, NEW_STR("samples")).ToLocalChecked());
+    size_t len = arr->Length();
     for (size_t i = 0; i < len; ++i) {
-        Local<Object> Item = Arr->Get(context, i).ToLocalChecked().As<Object>();
-        Sample s = unpack_sample(isolate, context, Item);
+        Local<Object> item = arr->Get(context, i).ToLocalChecked().As<Object>();
+        Sample s = unpack_sample(isolate, context, item);
         loc.samples.push_back(s);
     }
 
     return loc;
 }
 
-void calc_results(std::vector<Location>& locations, std::vector<RainResult>& results) {
+void unpack_locations(Isolate* isolate,
+                      const Local<Context>& context,
+                      const Local<Array>& input,
+                      std::vector<Location>& locations) {
+    size_t len = input->Length();
+    for (size_t i = 0; i < len; ++i) {
+        Local<Object> item = input->Get(context, i).ToLocalChecked().As<Object>();
+        locations.push_back(unpack_location(isolate, context, item));
+    }
+}
+
+void calc_results(std::vector<Location>& locations,
+                  std::vector<RainResult>& results) {
     results.resize(locations.size());
     std::transform(
         locations.begin(),
@@ -67,25 +83,47 @@ void calc_results(std::vector<Location>& locations, std::vector<RainResult>& res
         calc_rain_stats);
 }
 
-Local<Object> pack_rain_result(Isolate* isolate, const Local<Context>& context, const RainResult& result) {
-    Local<Object> Obj = Object::New(isolate);
+Local<Object> pack_rain_result(Isolate* isolate,
+                               const Local<Context>& context,
+                               const RainResult& result) {
+    Local<Object> obj = Object::New(isolate);
 
-    Obj->Set(context, NEW_STR("mean"), Number::New(isolate, result.mean));
-    Obj->Set(context, NEW_STR("median"), Number::New(isolate, result.median));
-    Obj->Set(context, NEW_STR("stdev"), Number::New(isolate, result.stdev));
-    Obj->Set(context, NEW_STR("n"), Integer::New(isolate, result.n));
+    obj->Set(context, NEW_STR("mean"), Number::New(isolate, result.mean));
+    obj->Set(context, NEW_STR("median"), Number::New(isolate, result.median));
+    obj->Set(context, NEW_STR("stdev"), Number::New(isolate, result.stdev));
+    obj->Set(context, NEW_STR("n"), Integer::New(isolate, result.n));
 
-    return Obj;
+    return obj;
+}
+
+Local<Array> pack_results(Isolate* isolate,
+                          const Local<Context>& context,
+                          const std::vector<RainResult>& results) {
+    Local<Array> arr = Array::New(isolate);
+
+    size_t len = results.size();
+    for (unsigned i = 0; i < len; ++i) {
+        Local<Object> obj = pack_rain_result(isolate, context, results[i]);
+        arr->Set(context, i, obj);
+    }
+
+    return arr;
 }
 
 void AvgRainfall(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     Local<Context> context = isolate->GetCurrentContext();
 
-    Local<Object> Obj = Local<Object>::Cast(args[0]);
-    Location loc = unpack_location(isolate, context, Obj);
+    // Args
+    Local<Object> obj = Local<Object>::Cast(args[0]);
+
+    // Unpack location
+    Location loc = unpack_location(isolate, context, obj);
+
+    // Calc
     double avg = avg_rainfall(loc);
 
+    // Return
     args.GetReturnValue().Set(avg);
 }
 
@@ -93,37 +131,42 @@ void RainfallData(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     Local<Context> context = isolate->GetCurrentContext();
 
-    Local<Object> Obj = Local<Object>::Cast(args[0]);
-    Location loc = unpack_location(isolate, context, Obj);
+    // Args
+    Local<Object> obj = Local<Object>::Cast(args[0]);
+
+    // Unpack location
+    Location loc = unpack_location(isolate, context, obj);
+
+    // Calc result
     RainResult result = calc_rain_stats(loc);
 
-    Local<Object> Result = pack_rain_result(isolate, context, result);
-    args.GetReturnValue().Set(Result);
+    // Pack result
+    Local<Object> output = pack_rain_result(isolate, context, result);
+
+    // Return
+    args.GetReturnValue().Set(output);
 }
 
 void CalcResults(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     Local<Context> context = isolate->GetCurrentContext();
 
+    // Args
+    Local<Array> input = Local<Array>::Cast(args[0]);
+
+    // Unpack locations
     std::vector<Location> locations;
+    unpack_locations(isolate, context, input, locations);
 
-    Local<Array> Input = Local<Array>::Cast(args[0]);
-    size_t len = Input->Length();
-    for (size_t i = 0; i < len; ++i) {
-        Local<Object> Item = Input->Get(context, i).ToLocalChecked().As<Object>();
-        locations.push_back(unpack_location(isolate, context, Item));
-    }
-
+    // Calc results
     std::vector<RainResult> results;
     calc_results(locations, /*out*/results);
 
-    Local<Array> Results = Array::New(isolate);
-    for (unsigned i = 0; i < len; ++i) {
-        Local<Object> Result = pack_rain_result(isolate, context, results[i]);
-        Results->Set(context, i, Result);
-    }
+    // Pack results
+    Local<Array> output = pack_results(isolate, context, results);
 
-    args.GetReturnValue().Set(Results);
+    // Return
+    args.GetReturnValue().Set(output);
 }
 
 void CalcResultsSync(const FunctionCallbackInfo<Value>& args) {
@@ -140,35 +183,27 @@ void CalcResultsSync(const FunctionCallbackInfo<Value>& args) {
         return;
     }
 
+    // Args
+    Local<Array> input = Local<Array>::Cast(args[0]);
+    Local<Function> callback = Local<Function>::Cast(args[1]);
+
     // Unpack locations
     std::vector<Location> locations;
-
-    Local<Array> Input = Local<Array>::Cast(args[0]);
-    size_t len = Input->Length();
-    for (size_t i = 0; i < len; ++i) {
-        Local<Object> Item = Input->Get(context, i).ToLocalChecked().As<Object>();
-        locations.push_back(unpack_location(isolate, context, Item));
-    }
-
-    // Callback
-    Local<Function> callback = Local<Function>::Cast(args[1]);
+    unpack_locations(isolate, context, input, locations);
 
     // Calc results
     std::vector<RainResult> results;
     calc_results(locations, /*out*/results);
 
     // Pack results
-    Local<Array> Results = Array::New(isolate);
-    for (unsigned i = 0; i < len; ++i) {
-        Local<Object> Result = pack_rain_result(isolate, context, results[i]);
-        Results->Set(context, i, Result);
-    }
+    Local<Array> output = pack_results(isolate, context, results);
 
     // Call callback
-    Local<Value> argv[] = { Results };
+    Local<Value> argv[] = { output };
     const unsigned argc = sizeof(argv) / sizeof(argv[0]);
     callback->Call(context, Null(isolate), argc, argv).ToLocalChecked();
 
+    // Return `undefined`
     std::cerr << "Returning from C++ now" << std::endl;
     args.GetReturnValue().Set(Undefined(isolate));
 }
@@ -203,15 +238,10 @@ static void WorkAsyncDone(uv_work_t* req, int status) {
     Local<Function> callback = Local<Function>::New(isolate, work->callback);
 
     // Pack results
-    Local<Array> Results = Array::New(isolate);
-    size_t len = results.size();
-    for (unsigned i = 0; i < len; ++i) {
-        Local<Object> Result = pack_rain_result(isolate, context, results[i]);
-        Results->Set(context, i, Result);
-    }
+    Local<Array> output = pack_results(isolate, context, results);
 
     // Call callback
-    Local<Value> argv[] = { Null(isolate), Results };
+    Local<Value> argv[] = { Null(isolate), output };
     const unsigned argc = sizeof(argv) / sizeof(argv[0]);
     callback->Call(context, Null(isolate), argc, argv).ToLocalChecked();
 
@@ -234,18 +264,13 @@ void CalcResultsAsync(const FunctionCallbackInfo<Value>& args) {
         return;
     }
 
+    // Args
+    Local<Array> input = Local<Array>::Cast(args[0]);
+    Local<Function> callback = Local<Function>::Cast(args[1]);
+
     // Unpack locations
     std::vector<Location> locations;
-
-    Local<Array> Input = Local<Array>::Cast(args[0]);
-    size_t len = Input->Length();
-    for (size_t i = 0; i < len; ++i) {
-        Local<Object> Item = Input->Get(context, i).ToLocalChecked().As<Object>();
-        locations.push_back(unpack_location(isolate, context, Item));
-    }
-
-    // Callback
-    Local<Function> callback = Local<Function>::Cast(args[1]);
+    unpack_locations(isolate, context, input, locations);
 
     // Spawn worker thread
     Work* work = new Work();
@@ -255,6 +280,7 @@ void CalcResultsAsync(const FunctionCallbackInfo<Value>& args) {
 
     uv_queue_work(uv_default_loop(), &work->request, WorkAsync, WorkAsyncDone);
 
+    // Return `undefined` immediately
     std::cerr << "Returning from C++ now" << std::endl;
     args.GetReturnValue().Set(Undefined(isolate));
 }
