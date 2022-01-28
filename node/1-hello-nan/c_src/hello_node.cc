e094b68358f150c15ecdec71eb711edb7263b5fd
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-01-28 19:29:35 +0300

    Simplify

--- node/1-hello-nan/c_src/hello_node.cc
+++ node/1-hello-nan/c_src/hello_node.cc
@@ -1,9 +1,9 @@
 #include <nan.h>
 
-using v8::FunctionTemplate;
 using v8::Local;
 using v8::String;
 
+#define NEW_FUN(fun) Nan::GetFunction(Nan::New<v8::FunctionTemplate>(fun)).ToLocalChecked()
 #define NEW_STR(str) Nan::New<v8::String>(str).ToLocalChecked()
 
 NAN_METHOD(Hello) {
@@ -12,9 +12,7 @@ NAN_METHOD(Hello) {
 }
 
 NAN_MODULE_INIT(Init) {
-    Nan::Set(target,
-             NEW_STR("hello"),
-             Nan::GetFunction(Nan::New<FunctionTemplate>(Hello)).ToLocalChecked());
+    Nan::Set(target, NEW_STR("hello"), NEW_FUN(Hello));
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
