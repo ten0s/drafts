0c2d4ecce4e1834daa1089191a4800e3ffc7d1b4
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-06-16 17:48:44 +0300

    Add gvalue example

---
+++ gobject/gvalue.c
@@ -0,0 +1,20 @@
+#include <glib-object.h>
+#include <glib/gprintf.h>
+
+int main (int argc, char *argv[])
+{
+  // GValues must be initialized
+  GValue a = G_VALUE_INIT;
+  const gchar *message;
+
+  // The GValue starts empty
+  g_assert (!G_VALUE_HOLDS_STRING (&a));
+
+  // Put a string in it
+  g_value_init (&a, G_TYPE_STRING);
+  g_assert (G_VALUE_HOLDS_STRING (&a));
+  g_value_set_static_string (&a, "Hello, world!");
+  g_printf ("%s\n", g_value_get_string (&a));
+
+  return 0;
+}
