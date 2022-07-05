13610d855b7c128e1f481e62ac98b5f7aca088e6
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-05 15:05:01 +0300

    Cleanup

--- gobject/gvalue.c
+++ gobject/gvalue.c
@@ -1,31 +1,31 @@
 #include <glib-object.h>
 #include <glib/gprintf.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  g_printf("G_TYPE_STRING: %ld\n"
-           "G_TYPE_INT   : %ld\n", G_TYPE_STRING, G_TYPE_INT);
+    g_printf("G_TYPE_STRING: %ld\n"
+             "G_TYPE_INT   : %ld\n", G_TYPE_STRING, G_TYPE_INT);
 
-  // GValues must be initialized
-  GValue a = G_VALUE_INIT;
+    // GValues must be initialized
+    GValue a = G_VALUE_INIT;
 
-  // The GValue starts empty
-  g_assert(!G_VALUE_HOLDS_STRING (&a));
+    // The GValue starts empty
+    g_assert(!G_VALUE_HOLDS_STRING (&a));
 
-  // Put a string in it
-  g_value_init(&a, G_TYPE_STRING);
-  g_assert(G_VALUE_HOLDS_STRING (&a));
+    // Put a string in it
+    g_value_init(&a, G_TYPE_STRING);
+    g_assert(G_VALUE_HOLDS_STRING (&a));
 
-  g_value_set_string(&a, "Hello, world!");
-  g_printf("%s\n", g_value_get_string(&a));
+    g_value_set_string(&a, "Hello, world!");
+    g_printf("%s\n", g_value_get_string(&a));
 
-  // Release all resources associated with this GValue
-  g_value_unset(&a);
+    // Release all resources associated with this GValue
+    g_value_unset(&a);
 
-  g_value_init(&a, G_TYPE_INT);
-  g_value_set_int(&a, 42);
-  g_printf("%d\n", g_value_get_int(&a));
-  g_value_unset(&a);
+    g_value_init(&a, G_TYPE_INT);
+    g_value_set_int(&a, 42);
+    g_printf("%d\n", g_value_get_int(&a));
+    g_value_unset(&a);
 
-  return 0;
+    return 0;
 }
