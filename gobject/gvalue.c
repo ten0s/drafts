09a0a9b8be5ca5907b9a9ad2e35da118bf77d257
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-07 12:06:48 +0300

    Finish example

--- gobject/gvalue.c
+++ gobject/gvalue.c
@@ -35,17 +35,46 @@ int main(int argc, char *argv[])
     //gchar **gdk_argv = NULL;
     //gdk_init(&gdk_argc, &gdk_argv);
 
-    GdkRGBA rgba = { .red = 0.5, .blue = 0.5, .green = 0.5, .alpha = 0.5 };
+    /*
+      https://github.com/ten0s/node-gtk
+      See gvalue-from-gir.c
 
-    GValue b = G_VALUE_INIT;
+      const color0 = new Gdk.RGBA({ red: 0.5, blue: 0.5, green: 0.5, alpha: 0.5 })
+      const type = GObject.typeFromName('GdkRGBA')
+
+      const val = new GObject.Value()
+      val.init(type)
+      val.setBoxed(color0)
+
+      color0.toString() => 'rgba(128,128,128,0.5)'
+      const color1 = val.getBoxed()
+      color1.toString() => 'rgba(128,128,128,0.5)'
+   */
+
+    GdkRGBA color0 = { .red = 0.5, .blue = 0.5, .green = 0.5, .alpha = 0.5 };
 
     GType type1 = gdk_rgba_get_type();
     g_printf("GdkRGBA type1: %ld\n", type1);
 
-    // Returns 0 without the call above :(
+    // Returns 0 without the call above.
     // Works if loaded from GIR. See gvalue-from-gir.c
     GType type2 = g_type_from_name("GdkRGBA");
     g_printf("GdkRGBA type2: %ld\n", type2);
+
+    GValue b = G_VALUE_INIT;
+    g_value_init(&b, type1);
+    g_value_set_boxed(&b, &color0);
+
+    gchar *str0 = gdk_rgba_to_string(&color0);
+    g_printf("GdkRGBA str0: %s\n", str0);
+
+    GdkRGBA *color1 = (GdkRGBA *)g_value_get_boxed(&b);
+    gchar *str1 = gdk_rgba_to_string(color1);
+    g_printf("GdkRGBA str1: %s\n", str1);
+
+    g_free(str0);
+    g_free(str1);
+    g_value_unset(&b);
 
     return 0;
 }
